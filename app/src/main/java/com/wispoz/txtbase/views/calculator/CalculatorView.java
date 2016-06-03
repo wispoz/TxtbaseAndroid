ef299ccc5ccd6d561e6c2caea207c72908ef0edf
wispoz <wispoz@inbox.ru>
2016-06-03 18:30:44 +0300

    MVVM

--- app/src/main/java/com/wispoz/txtbase/views/calculator/CalculatorView.java
+++ app/src/main/java/com/wispoz/txtbase/views/calculator/CalculatorView.java
@@ -18,9 +18,10 @@ public class CalculatorView extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.calculator, container, false);
-        CalculatorBinding binding = DataBindingUtil.setContentView(getActivity(),R.layout.calculator);
+        CalculatorBinding binding = DataBindingUtil.inflate(inflater, R.layout.calculator, container, false);
+       // View view = binding.getRoot();
         PlaceCalculator place = new PlaceCalculator("13","15");
         binding.setCalculator(place);
-        return view;
+        return binding.getRoot();
     }
 }
