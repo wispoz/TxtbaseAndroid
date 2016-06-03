88b502090194911687244f65d513e02fd83fd5d4
wispoz <wispoz@inbox.ru>
2016-06-03 18:31:14 +0300

    MVVM

--- app/src/main/java/com/wispoz/txtbase/models/PlaceCalculator.java
+++ app/src/main/java/com/wispoz/txtbase/models/PlaceCalculator.java
@@ -4,8 +4,8 @@ package com.wispoz.txtbase.models;
  * Created by wispoz on 03.06.16.
  */
 public class PlaceCalculator {
-    public final String place;
-    public final String from;
+    public  String place;
+    public  String from;
     public PlaceCalculator(String place, String from) {
         this.place = place;
         this.from = from;
